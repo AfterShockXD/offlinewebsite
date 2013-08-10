b4bc38fb06ceb11842092128be725f02c63da9e8
ToxicDeath <Toxicdeath2@live.com>
2013-08-10 21:16:45 +0200

    Testing

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include("classes/config.php");
 include("classes/functions.php");
 ?>
@@ -7,12 +7,13 @@ include("classes/functions.php");
 <head>
 <!-- InstanceBeginEditable name="doctitle" -->
 <title>Home Page</title>
+<!-- Test -->
 <!-- InstanceEndEditable -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     <!--scripts-->
-    
+
     <script src="js/jquery-1.9.1.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
@@ -26,14 +27,14 @@ include("classes/functions.php");
     </style>
   <meta charset="utf-8">
   <!-- InstanceBeginEditable name="head" -->
-    
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <meta charset="utf-8">
 <!-- InstanceEndEditable -->
 </head>
 <body>
-   
+
   <div class="navbar navbar-static-top">
     		<div  class="navbar-inner ">
             	<a  class="brand" href="#"><strong>Gamers Connected</strong></a>
@@ -50,7 +51,7 @@ include("classes/functions.php");
                     <li><a href="downloads.php">Downloads</a></li>
                     <li class="divider-vertical"></li>
             	</ul>
-              
+
               <ul class="nav pull-right">
   				<li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -60,8 +61,8 @@ include("classes/functions.php");
                 <ul class="dropdown-menu">
                		<div align="center"><img style="width:100px; height:100px;" src="img/png.png" class="img-circle"  ></div>
                		<div align="center"><strong>Name Surname</strong></div>
-               
-               
+
+
                       <li class="divider"></li>
                       <li><a href="#"><i class="icon-lock"></i> Login</a></li>
                       <li><a href="#"><i class="icon-user"></i> Register</a></li>
@@ -73,7 +74,7 @@ include("classes/functions.php");
               </ul>
     		</div>
      </div>
-       
+
        <div style="padding-top:5px" class="container">
 	   <!-- InstanceBeginEditable name="main" -->
 
@@ -81,13 +82,13 @@ include("classes/functions.php");
  		<h1>Welcome</h1>
   		<p>To Gamers Connected, this is an official offline website where you can be up to date on whats happening at the event and where you can get some support if you are stuck.All the Game Servers Ip's Will be on this website under the Servers TAB, This website has an built in Support system where you can chat with admin to help you.</p>
   		<p>
-    
+
         <!-- LiveZilla Chat Button Link Code (ALWAYS PLACE IN BODY ELEMENT) --><a href="javascript:void(window.open('http://127.0.0.1/offlinewebsite/LiveZilla/chat.php?acid=42e4e','','width=590,height=760,left=0,top=0,resizable=yes,menubar=no,location=no,status=yes,scrollbars=yes'))" class="lz_cbl"><img src="http://127.0.0.1/offlinewebsite/LiveZilla/image.php?acid=4d7eb&amp;id=3&amp;type=inlay" width="200" height="50" style="border:0px;" alt="LiveZilla Live Chat Software"></a><!-- http://www.LiveZilla.net Chat Button Link Code --><!-- LiveZilla Tracking Code (ALWAYS PLACE IN BODY ELEMENT) --><div id="livezilla_tracking" style="display:none"></div><script type="text/javascript">
         var script = document.createElement("script");script.async=true;script.type="text/javascript";var src = "http://127.0.0.1/offlinewebsite/LiveZilla/server.php?acid=294b6&request=track&output=jcrpt&nse="+Math.random();setTimeout("script.src=src;document.getElementById('livezilla_tracking').appendChild(script)",1);</script><noscript><img src="http://127.0.0.1/offlinewebsite/LiveZilla/server.php?acid=294b6&amp;request=track&amp;output=nojcrpt" width="0" height="0" style="visibility:hidden;" alt=""></noscript><!-- http://www.LiveZilla.net Tracking Code -->
-            
+
   		</p>
 	</div>
-       
+
     <div class="container">
       <div class="row-fluid">
           <div class="span6">
@@ -116,7 +117,7 @@ include("classes/functions.php");
                   </li>
                 </ul>
             <h2></h2>
-            
+
           </div>
           <div class="span6">
           <h2>Upcoming Games:</h2>
@@ -158,11 +159,11 @@ include("classes/functions.php");
             </tr>
           </table>
           <p>&nbsp;</p>
-          
-          
-          
-          
-          
+
+
+
+
+
           </div>
 	</div>
     <div>
@@ -196,11 +197,11 @@ include("classes/functions.php");
                     </tr>
             </table>
         </div>
-    
+
 <!-- InstanceEndEditable -->
-       
+
 
 		</div>
-    
+
 </body>
 <!-- InstanceEnd --></html>

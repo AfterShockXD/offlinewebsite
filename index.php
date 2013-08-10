3eb23be6341a31f168a296db0d7071aca387710b
ToxicDeath <Toxicdeath2@live.com>
2013-08-10 21:19:10 +0200

    Removed the testing message starting implementation of the countdown timer

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ include("classes/functions.php");
 <head>
 <!-- InstanceBeginEditable name="doctitle" -->
 <title>Home Page</title>
-<!-- Test -->
 <!-- InstanceEndEditable -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->

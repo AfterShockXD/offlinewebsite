d7b3375d2912f9024697fd31a1018dc51a9cfa27
AfterShockXD <wow.jasonz@gmail.com>
2013-08-14 15:24:38 +0200

    Download
    added Downloads

--- downloads.php
+++ downloads.php
@@ -51,6 +51,54 @@
               </ul>
     		</div>
   </div>
+  
+  <div style="padding-top:10%" class="container">
+  <div style="float:left">
+  <ul class="thumbnails">
+  <li class="span4">
+    <div class="thumbnail well">
+      <img src="img/Dc++image.png" alt="">
+      <h3>Strong DC++</h3>
+      <p>StrongDC++ is a client for sharing files in Direct Connect network by using NMDC and ADC protocols</p>
+      <br />
+      <div align="center"><button class="btn-large btn-danger">Download</button></div>
+    </div>
+  </li>
+</ul>
+</div>
+ <div style="float:left; padding-left:4px; width:300px; height:399px">
+  <ul class="thumbnails">
+  <li class="span4">
+    <div class="thumbnail well">
+      <img src="img/TeamSpeak3.png" alt="">
+      <h3>Teamspeak 3 Client</h3>
+      <p>TeamSpeak 3 continues the legacy of the original TeamSpeak communication system </p>
+      <br />
+      <br />
+      <div align="center"><button class="btn-large btn-danger">Download</button></div>
+    </div>
+  </li>
+</ul>
+</div>
+   <div style="float:left; padding-left:4px;">
+  <ul class="thumbnails">
+  <li class="span4">
+    <div class="thumbnail well">
+      <img src="img/Avast.png" alt="">
+      <h3>Free Avast Antivirus</h3>
+      <p>Free Antivirus and anti-spyware protection for Windows 8, 7, Vista, and XP. Best free antivirus with better detection</p>
+      <br />
+      <div align="center"><button class="btn-large btn-danger">Download</button></div>
+    </div>
+  </li>
+</ul>
+</div>
+  
+  
+  
+  
+  
+  </div>
    
   <script src="http://code.jquery.com/jquery.js"></script>
   <script src="js/bootstrap.min.js"></script>
